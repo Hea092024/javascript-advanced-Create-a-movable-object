e8d7fc92e1a9044a462879f9ddcec3b8ca21778a
Hea092024 <Hea092024@gmail.com>
2025-01-07 14:05:51 +0100

    added movement up + down

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const pos = { x: 0, y: 0 };
 const move = (e) => (capybara.style.translate = `${pos.x}px ${pos.y}px`);
 
 const keyActions = {
+  ArrowUp: () => move((pos.y -= 40)),
+  ArrowDown: () => move((pos.y += 40)),
   ArrowLeft: () => move((pos.x -= 40)),
   ArrowRight: () => move((pos.x += 40)),
 };
