27fa4e052710b6144ec2d87eeba4c80b92edcee2
Hea092024 <Hea092024@gmail.com>
2025-01-07 14:55:25 +0100

    edited comment in js

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const capybara = document.querySelector("#move");
 const pos = { x: 0, y: 0 };
 
 const move = (e) => (capybara.style.translate = `${pos.x}px ${pos.y}px`);
-//move is the function that moves the capybara
+//map of arrow keys
 const keyActions = {
   ArrowUp: () => move((pos.y -= 40)),
   ArrowDown: () => move((pos.y += 40)),
   ArrowLeft: () => move((pos.x -= 40)),
   ArrowRight: () => move((pos.x += 40)),
 };
+
 
 addEventListener("keydown", (evt) => {
   evt.preventDefault();
