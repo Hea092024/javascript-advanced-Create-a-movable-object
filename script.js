11314ca448efb1f573bc4893f8014c410876d921
Hea092024 <Hea092024@gmail.com>
2025-01-08 12:00:59 +0100

    edited css + added mouseclick to js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const capybara = document.querySelector("#move");
 //pos is the position of the capybara
 const pos = { x: 0, y: 0 };
 
-const move = (e) => (capybara.style.translate = `${pos.x}px ${pos.y}px`);
+const move = (evt) => (capybara.style.translate = `${pos.x}px ${pos.y}px`);
 //map of arrow keys
 const keyActions = {
   ArrowUp: () => move((pos.y -= 40)),
@@ -17,4 +17,13 @@ addEventListener("keydown", (evt) => {
   evt.preventDefault();
   // if (!evt.repeat) //does not move when key is held, removing this will make it move when key is held
      keyActions[evt.key]?.();
+});
+
+// moves capybara to the clicked position
+addEventListener("click", (evt) => {
+  // Get click coordinates 
+  pos.x = evt.clientX;
+  pos.y = evt.clientY;
+  
+  move();
 });
