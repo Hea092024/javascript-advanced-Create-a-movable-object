2c546b8454079354c205735fb69893f3b3147df5
Hea092024 <Hea092024@gmail.com>
2025-01-10 15:28:08 +0100

    added window height + width

--- script.js
+++ script.js
@@ -34,3 +34,12 @@ let welcome = document.getElementById("title");
 let greeting = "Welcome to Capybara Land";
 
 welcome.textContent = greeting;
+
+let vw = Math.max(
+  document.documentElement.clientWidth || 0,
+  window.innerWidth || 0
+);
+let vh = Math.max(
+  document.documentElement.clientHeight || 0,
+  window.innerHeight || 0
+);
